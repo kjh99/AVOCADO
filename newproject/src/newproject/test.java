e428efc4ff74e9290a71c970b35e9f717db35da2
tokiot <wlsgh3945@gmail.com>
2023-05-31 16:23:46 +0900

    0531

---
+++ newproject/src/newproject/test.java
@@ -0,0 +1,11 @@
+package newproject;
+
+public class test {
+	public static void main(String[] args) {
+		boolean new1 =new MysqlConnection().hasId("oop21");
+		System.out.println(new1);
+		
+		
+	}
+
+}
